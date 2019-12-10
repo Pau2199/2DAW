c73e58b365bf8b913108feec674fd2c6bd29625c
Pau <pau@gmail.com>
2019-12-10 18:04:24 +0100

    Ejercicio 4 php finalizado y ejercico 5 php

---
+++ Desarrollo-Web-Entorno-Servidor/Primer_Trimestre/Tema-6/Ejercicios/Ejercicio-5/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="estilo.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <?php require_once 'cabeceraMenu.php' ?>
+    <img src="topsecret.png" alt="topSecret">
+</body>
+</html>
---
+++ Desarrollo-Web-Entorno-Servidor/Primer_Trimestre/Tema-6/Ejercicios/Ejercicio-5/registro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="registro.php">
+            <fieldset>
+               <legend>Datos Personales</legend>
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" id="nombre">
+                <label for="apellido1">Primer Apellido</label>
+                <input type="text" name="apellido1" id="appelido1">
+                <label for="apellido2">Apellido</label>
+                <input type="text" name="apellido2" id="apellido2">
+                <label for="FechaNacimiento">Fecha de Nacimiento</label>
+                <input type="text" name="nombre" id="fnacimiento">
+            </fieldset>
+            <fieldset></fieldset>
+
+
+        </form>
+    </body>
+</html>
