c847b7aa6ba2aef005598243d80290abb86575b8
Pau <pau@gmail.com>
2019-12-19 18:45:38 +0100

    ahorcado

--- Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/Tema-10/Actividades/Actividad-6/ahorcado/palabraAleatoria.php
+++ Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/Tema-10/Actividades/Actividad-6/ahorcado/palabraAleatoria.php
@@ -5,11 +5,10 @@ $dificultad='';
 if($_POST['dificultad'] == 0){
     $dificultad = '<=5';
 }else if($_POST['dificultad'] == 1){
-    $dificultad = '>=6 AND <=9';
+    $dificultad = '>=6 AND LENGTH(sin_acentos)<=9';
 }else{
     $dificultad = '>=7';
 }
-//SELECT sin_acentos FROM palabras WHERE LENGTH(sin_acentos)>=6 AND LENGTH(sin_acentos)<=9 ORDER BY RAND() LIMIT 1 
 require_once 'conexionBD.php';
 $consulta = $conexion->query('SELECT sin_acentos FROM palabras WHERE LENGTH(sin_acentos)'.$dificultad.' ORDER BY RAND() LIMIT 1 ;');
 $registro = $consulta->fetch(PDO::FETCH_NUM);
