95062d551efb350bb43d01a4dd0033cbcd304c48
Pau <pau_ll_m@hotmail.com>
2019-12-19 02:31:39 +0100

    ahorcado cliente

--- Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/Tema-10/Actividades/Actividad-6/ahorcado/js/ahorcado.js
+++ Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/Tema-10/Actividades/Actividad-6/ahorcado/js/ahorcado.js
@@ -1,3 +1,4 @@
+//https://hdeleon.net/como-obtener-un-hijo-por-su-posicion-con-jquery-nth-child/
 $(function(){
     $.getJSON('obtenerLetras.php', function (data){
         console.log(data);
@@ -12,18 +13,51 @@ $(function(){
     $('#bbdd').click(function(){
         $.getJSON('palabraAleatoria.php', function (data){
             console.log(data);
-            empezarPartida(data);
+            empezarPartida(data[0]);
         });
     });
     $('#empezar').click(function(evento){
         evento.preventDefault();
-        var palabra = this.value.trim();
+        var palabra = $('#campopalabra').val().trim();
         if(palabra.length != 0){
             empezarPartida(palabra);
         }
     });
     function empezarPartida(palabra){
-        console.log('llega aqui ' + palabra)
+        console.log(palabra);
+        $('#menuInicio').css('display', 'none');
+        $('#partidaPersonalizada').css('display', 'none');
+        $('#menuPartida').css('display', 'block');
+        $('#laPalabra').css('display', 'block');
+        $('#filaLetras').empty();
+        for (var i = 0 ; i<palabra.length ; i++){
+            $('#filaLetras').append($('<td>'));
+        }
+
+        var intento = 1;
+        var aciertos = 0;
+        $('.grid-letra').click(function(){
+            var letraAcertada = false;
+            $(this).css('background-color', 'red');
+            $(this).off();
+            for (var i = 0 ; i<palabra.length ; i++){
+                if(palabra[i] == $(this).html()){
+                    $('#filaLetras td:nth-child('+(i+1)+')').append($(this).html());
+                    letraAcertada = true;
+                    aciertos++;
+                }
+            }
+            if(letraAcertada == false){
+                intento++;
+                $('img').attr('src', 'img/'+intento+'.png');
+            }else{
+                if(aciertos == palabra.length){
+                    $('img').attr('src', 'img/win.png');
+                }
+            }
+
+
+        })
     }
 
 
