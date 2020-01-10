a5bf9c2a8ae65685c105a61c9390382623f5486a
Pau <pau@gmail.com>
2020-01-10 18:07:53 +0100

    PLUMBUS

--- Desarrollo-Web-Entorno-Cliente/plumbus/js/plumbus.js
+++ Desarrollo-Web-Entorno-Cliente/plumbus/js/plumbus.js
@@ -48,9 +48,48 @@ function create (){
     platforms.create(60, 250, 'plataforma').setScale(.5).refreshBody();
     platforms.create(750, 220, 'plataforma').setScale(.5).refreshBody();
     platforms.create(600, 400, 'plataforma').setScale(.5).refreshBody();
+
+    player = this.physics.add.sprite(100, 450, 'morty');
+    player.setBounce(0);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key:'derecha',
+        frames: this.anims.generateFrameNumbers('morty', {start:5, end:8}),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key:'izquierda',
+        frames: this.anims.generateFrameNumbers('morty', {start:3, end:0}),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key:'parado',
+        frames: [{key:'morty', frame: 4}],
+        frameRate: 10,
+        repeat: -1
+    });
     
+    this.physics.add.collider(platforms, player);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(){
+    if(cursors.left.isDown){
+        player.anims.play('izquierda', true);
+        player.setVelocityX(-100);
+    }else if(cursors.right.isDown){
+        player.anims.play('derecha', true);
+        player.setVelocityX(100);
+    }else{
+        player.anims.play('parado', false);
+        player.setVelocityX(0);
+    }
+    
+    if(cursors.up.isDown && player.body.touching.down){
+        player.setVelocityY(-340);
+    }
 
 }
