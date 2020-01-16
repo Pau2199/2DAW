176c35cd40f6a0cde01c50bd0bdd4dc685ac2ef1
Pau <pau_ll_m@hotmail.com>
2020-01-16 01:09:46 +0100

    plumbus

--- Desarrollo-Web-Entorno-Cliente/plumbus/js/plumbus.js
+++ Desarrollo-Web-Entorno-Cliente/plumbus/js/plumbus.js
@@ -38,6 +38,7 @@ function preload(){
     this.load.image('plumbus', '/assets/plumbus.png');
     this.load.image('portal', '/assets/portal.png');
     this.load.image('timecop', '/assets/timecop.png');
+    this.load.image('portalgun', '/assets/portalgun.png');
     this.load.spritesheet('morty', '/assets/morty.png', {frameWidth: 32, frameHeight: 40});
 }
 
@@ -52,7 +53,7 @@ function create (){
     player = this.physics.add.sprite(100, 450, 'morty');
     player.setBounce(0);
     player.setCollideWorldBounds(true);
-    
+
     this.anims.create({
         key:'derecha',
         frames: this.anims.generateFrameNumbers('morty', {start:5, end:8}),
@@ -71,12 +72,12 @@ function create (){
         frameRate: 10,
         repeat: -1
     });
-    
+
     this.physics.add.collider(platforms, player);
     cursors = this.input.keyboard.createCursorKeys();
-    
+
     plumbus = this.physics.add.group({
-       key:'plumbus',
+        key:'plumbus',
         repeat: 2
     });
     plumbus.children.iterate(function(child){
@@ -84,16 +85,22 @@ function create (){
     })
     this.physics.add.collider(platforms, plumbus);
     this.physics.add.overlap(plumbus, player, collectPlumbus, null, this);
-    
+
     scoreText = this.add.text(16,54,'Score: ' + score, {fontSize: '32px', fill: '#000'});
-    
-    portalgun = this.physics.add.image(Phaser.Math.Between(10, 799), posicionesY[Phaser.Math.Between(0,2)], 'portalgun');
+    levelText = this.add.text(16,17,'Nivel: ' + nivel, {fontSize: '32px', fill: '#000'});
+
+    portalgun = this.physics.add.image(Phaser.Math.Between(10, 790), posicionesY[Phaser.Math.Between(0,2)], 'portalgun');
     portal = this.physics.add.image(400,400, 'portal');
     portal.disableBody(true, true);
     this.physics.add.collider(platforms, portal);
     this.physics.add.collider(platforms, portalgun);
-    this.physics.add.overlap(platforms, portal, hitportal, null, this);
-    this.physics.add.overlap(platforms, portalgun, hitPistola, null, this);
+    this.physics.add.overlap(player, portal, hitPortal, null, this);
+    this.physics.add.overlap(player, portalgun, hitPistola, null, this);
+
+    timecops = this.physics.add.group();
+    this.physics.add.collider(platforms, timecops);
+    this.physics.add.overlap(player, timecops, hitTimecops, null, this);
+
 }
 
 function update(){
@@ -107,9 +114,13 @@ function update(){
         player.anims.play('parado', false);
         player.setVelocityX(0);
     }
-    
+
     if(cursors.up.isDown && player.body.touching.down){
         player.setVelocityY(-340);
+    }
+
+    if(gameOver){
+        return;
     }
 
 }
@@ -126,4 +137,36 @@ function collectPlumbus(player, plumb){
     scoreText.setText('Score: ' + score);
 }
 
-function hitPistola
+function hitPistola(player, pistola){
+    pistola.disableBody(true, true);
+    portal.enableBody(true, Phaser.Math.Between(10,790), posicionesY[Phaser.Math.Between(0,2)], true, true);
+}
+function hitPortal(player, elPortal){
+    nivel++;
+
+    elPortal.disableBody(true, true);
+    player.disableBody(true, true);
+
+    plumbus.children.iterate(function (child){
+        child.enableBody(true, child.x, 0, true , true);
+        createPlumbus(child);
+    });
+    player.enableBody(true, Phaser.Math.Between(10, 790), posicionesY[Phaser.Math.Between(0,2)], true , true);
+    portalgun.enableBody(true, Phaser.Math.Between(10, 790), posicionesY[Phaser.Math.Between(0,2)], true , true);
+    levelText.setText('Nivel: ' + nivel);
+    if(nivel%2 == 0){
+        var x = (player.x<400) ? Phaser.Math.Between(400,800) : Phaser.Math.Between(0,400);
+        var timecop = timecops.create(x, 16, 'timecop');
+        timecop.setBounce(1);
+        timecop.setCollideWorldBounds(true);
+        timecop.setVelocity(Phaser.Math.Between(-200, 200), 50);
+        timecop.allowGravity = false;   
+    }
+}
+
+function hitTimecops(player, timecops){
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    gameOver = true;
+}
