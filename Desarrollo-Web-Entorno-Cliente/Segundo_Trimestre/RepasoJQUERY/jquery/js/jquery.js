e6950b319cdf9c3bbc6194ef9106fc215bedd5bf
Pau <pau_ll_m@hotmail.com>
2020-01-28 00:02:58 +0100

    jquery repaso

--- Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/RepasoJQUERY/jquery/js/jquery.js
+++ Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/RepasoJQUERY/jquery/js/jquery.js
@@ -5,37 +5,40 @@ $(function(){
     })
 
     $('span').click(function(){
-        if($('#inputGenerado').length == 0){
-            localStorage.setItem('valorInput', $(this).html());
-            var input = $('<input>').attr({
-                type: 'text',
-                id: 'inputGenerado',
-                class: 'datosAlumno',
-                name: 'inputGenerado',
-                value: $(this).html()
-            });
-            $(this).html('')
-            $(this).append(input);
-            input.focus();
-            input.select();
-        }
+        localStorage.setItem('valorInput', $(this).html());
+        var input = $('<input>').attr({
+            type: 'text',
+            id: 'inputGenerado',
+            class: 'datosAlumno',
+            name: 'inputGenerado',
+            value: $(this).html()
+        });
+        $(this).html('')
+        $(this).append(input);
+        input.focus();
+        input.select();
     })
 
     $('div').on('blur', '.datosAlumno', function(){
-        $.post({url: 'modificarDato.php',
-                data: {idAlumno: localStorage.getItem('idAlumno'), valorNuevo: $('#inputGenerado').val(), campoParaModificar: $(this).attr('id')},
-                success: function(data){
-                    console.log(data);
-                    if(data.filasAfectadas != 0){
-                        $('#inputGenerado').parent().html($('#inputGenerado').val());
-                        $('#inputGenerado').remove();
-                    }else{
-                        $('#inputGenerado').parent().html(localStorage.getItem('valorInput'));
-                        $('#inputGenerado').remove();
-                    }
-                },
-                dataType: 'json'
-               })
+        if(($(this).val().length != 0) && ($(this).val() != localStorage.getItem('valorInput'))){
+            $.post({url: 'modificarDato.php',
+                    data: {idAlumno: localStorage.getItem('idAlumno'), valorNuevo: $('#inputGenerado').val(), campoParaModificar: $(this).parent().attr('id')},
+                    success: function(data){
+                        console.log(data);
+                        if(data.filasAfectadas != 0){
+                            $('#inputGenerado').parent().html($('#inputGenerado').val());
+                            $('#inputGenerado').remove();
+                        }else{
+                            $('#inputGenerado').parent().html(localStorage.getItem('valorInput'));
+                            $('#inputGenerado').remove();
+                        }
+                    },
+                    dataType: 'json'
+                   })
+        }else{
+            $('#inputGenerado').parent().html(localStorage.getItem('valorInput'));
+            $('#inputGenerado').remove();
+        }
     })
 
     $('td').click(function(){
@@ -49,6 +52,7 @@ $(function(){
                 value: $(this).html()
             });
             $(this).html('');
+            form.append(input);
             $(this).append(input);
             input.focus();
             input.select();   
@@ -56,19 +60,23 @@ $(function(){
     });
 
     $('tr').on('blur', '#inputGeneradoNotas', function(){
-        $.post({url: 'modificarNotas.php',
-                data:{idAlumno:2321, idActividad: $(this).parent().attr('class').split('act')[1], notaNueva: $('#inputGeneradoNotas').val()},
-                success: function(data){
-                    console.log(data);
-                    if(data.filasAfectadas != 0){
-                        $('#inputGeneradoNotas').parent().html($('#inputGeneradoNotas').val());
-                        $('#inputGeneradoNotas').remove();
-                    }else{
-                        $('#inputGeneradoNotas').parent().html(localStorage.getItem('nota'));
-                        $('#inputGeneradoNotas').remove();
+        if(($(this).val() >= 0) && ($(this).val() <= 10) && ($(this).val() != localStorage.getItem('nota'))){
+            $.post({url: 'modificarNotas.php',
+                    data:{idAlumno: $('#inputGeneradoNotas').attr('class').split('notas')[1], idActividad: $(this).parent().attr('class').split('act')[1], notaNueva: $('#inputGeneradoNotas').val()},
+                    success: function(data){
+                        if(data.filasAfectadas != 0){
+                            $('#inputGeneradoNotas').parent().html($('#inputGeneradoNotas').val());
+                            $('#inputGeneradoNotas').remove();
+                        }else{
+                            $('#inputGeneradoNotas').parent().html(localStorage.getItem('nota'));
+                            $('#inputGeneradoNotas').remove();
+                        }
                     }
-                }
-               })
+                   })
+        }else{
+            $('#inputGeneradoNotas').parent().html(localStorage.getItem('nota'));
+            $('#inputGeneradoNotas').remove();
+        }
     })
 
 })
--- Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/RepasoJQUERY/jquery/modificarDato.php
+++ Desarrollo-Web-Entorno-Cliente/Segundo_Trimestre/RepasoJQUERY/jquery/modificarDato.php
@@ -1,8 +1,4 @@
 <?php
-/*require_once 'conexionBD.php';
-$modificacion = $conexion->query('UPDATE alumnos SET '.$_POST['campoParaModificar'].' = "'.$_POST['valorNuevo'].'" WHERE id = '.$_POST['idAlumno'].';');
-$array = array('error' => $modificacion->execute());
-echo json_encode($array);*/
 require_once 'conexionBD.php';
 
 if($_POST['campoParaModificar'] == 'nombre' || $_POST['campoParaModificar'] == 'apellido1' || $_POST['campoParaModificar'] == 'apellido2' || $_POST['campoParaModificar'] == 'dni' || $_POST['campoParaModificar'] == 'fechaNacimiento' || $_POST['campoParaModificar'] == 'email' || $_POST['campoParaModificar'] == 'telefono'){
