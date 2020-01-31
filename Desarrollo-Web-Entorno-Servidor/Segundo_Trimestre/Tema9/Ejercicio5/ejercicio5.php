a19435b13841ac3408bde879432a50ae53d63850
Pau <pau@gmail.com>
2020-01-31 21:00:12 +0100

    ejercicio 5 phop

---
+++ Desarrollo-Web-Entorno-Servidor/Segundo_Trimestre/Tema9/Ejercicio5/ejercicio5.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_GET['Rick']))
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="#">
+            <button type="submit" name="Rick">Rick</button>
+            <button type="submit" name="Morty">Morty</button><br>
+            <input type="text" name="filtro">
+            <input type="submit" name="envioDatos" value="Realizar Busqueda">
+        </form>
+    </body>
+</html>
